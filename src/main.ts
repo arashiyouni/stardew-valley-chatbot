50daad84ecf9aa5fccc86fc7fea579c17f5bb3a9
arashiyouni <angie.espinoza.campos@gmail.com>
2025-06-29 21:18:34 -0600

    "🚨 Fix handler and tsconfig for Vercel deployment"

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import express from 'express';
+
+const expressApp = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.init();
+  // await app.listen(3000);
 }
 bootstrap();
+export const handler = (req, res) => {
+  expressApp(req, res);
+};
