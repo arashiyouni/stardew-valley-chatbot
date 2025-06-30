45f7b4f3fcd75710385d5337f135d002ce556837
arashiyouni <angie.espinoza.campos@gmail.com>
2025-06-29 19:31:19 -0600

    ✨ added envconfig into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OpenAiModule } from './open-ai/open-ai.module';
+import { EnvConfig } from './config/env';
 
 @Module({
-  imports: [],
+  imports: [OpenAiModule,
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      load: [EnvConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
