c320867340e89aa4bf455eb4ce4aa057adc556fe
arashiyouni <angie.espinoza.campos@gmail.com>
2025-06-29 21:33:41 -0600

    🚨 fix main and add gitignore vercel file

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import express from 'express';
+import { Request, Response } from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   app.enableCors();
   await app.init();
   // await app.listen(3000);
 }
 bootstrap();
-export const handler = (req, res) => {
+
+export const handler = (req: Request, res: Response) => {
   expressApp(req, res);
 };
